d1aaa6a8017203cb9791287833da32c5eee4d793
Lucas <lcseferreira@hotmail.com>
2022-05-15 07:55:08 -0300

    Configuração do banco MySQL e do JPA

--- src/main/java/com/kneladev/springboot2/domain/Movie.java
+++ src/main/java/com/kneladev/springboot2/domain/Movie.java
@@ -2,10 +2,20 @@ package com.kneladev.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data // Gera getter, setter, equals, hashcode, tostring..
 @AllArgsConstructor // Construtor com todos args
+@NoArgsConstructor // Construtor sem args
+@Entity // Entidade do banco
 public class Movie {
+    @Id // É um ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera automaticamente o valor
     private Long id;
 
     // @JsonProperty("name") nome da chave passado no body da requisição
