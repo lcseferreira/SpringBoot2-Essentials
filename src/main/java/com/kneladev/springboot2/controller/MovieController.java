4928df1968357f95a5cdf679c4808e0418124b86
Lucas <lcseferreira@hotmail.com>
2022-05-14 23:27:25 -0300

    Implementando o método PUT

--- src/main/java/com/kneladev/springboot2/controller/MovieController.java
+++ src/main/java/com/kneladev/springboot2/controller/MovieController.java
@@ -54,4 +54,10 @@ public class MovieController {
         movieService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Movie movie) {
+        movieService.replace(movie);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
